a3b26f3275c1e663e2483efdfac49d61df277af8
poojaghodmode <poojaghodmode@gmail.com>
2021-08-10 19:19:48 +0530

    Add for PWA

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration.js";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,4 +11,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-
+serviceWorkerRegistration.register();
